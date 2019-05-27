2d2a466cd3d8b5b233a3b61bbd955553cd120bb9
Benny Ho <benny.ho2530@gmail.com>
2019-05-27 10:13:38 +0200

    added onmouseover and onmouseout function

--- dom.js
+++ dom.js
@@ -1,1 +1,12 @@
 document.getElementById("doge").innerHTML = "Woof!";
+
+document.getElementById("doge").onmouseover = function() {mouseOver()};
+document.getElementById("doge").onmouseout = function() {mouseOut()};
+
+function mouseOver() {
+    document.getElementById("doge").innerHTML = "Woof Woof!";
+}
+
+function mouseOut() {
+    document.getElementById("doge").innerHTML = "Woof!";
+}
